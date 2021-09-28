1d90f63b3c311e9cd41cd41c87ff1c6b49fe4e33
mihaincd <danut49@gmail.com>
2021-09-28 16:13:24 +0300

    first commit for graphQL

--- src/features/helloWorld/HelloWorld.js
+++ src/features/helloWorld/HelloWorld.js
@@ -1,6 +1,16 @@
-function HelloWorld(){
-    return "Hello World !👋🐱‍🚀🐱‍🚀🐱‍👓🐱‍🚀"
+import React from 'react'
+import { useQuery } from "@apollo/client";
+import LoadingFakeText from "@bit/totalsoft_oss.react-mui.fake-text/dist/LoadingFakeText";
+import { MY_FIRST_QUERY } from "./queries/MyFirstQuery";
 
+function HelloWorld() {
+    const {loading, data} = useQuery(MY_FIRST_QUERY);
+
+    if(loading){
+        return <LoadingFakeText lines={10} />
+    }
+
+    return data?.myFirstEndpoint
 }
 
 export default HelloWorld;
